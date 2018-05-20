56a4750e7a1da34b33f07bcb17f1930e70e6ba53
alissa-huskey <alissa.huskey@gmail.com>
2018-05-19 22:31:05 -0600

    refactor code to split out modes into functions

--- src/main.m
+++ src/main.m
@@ -46,8 +46,48 @@ void set_default_handler(NSString *url_scheme, NSString *handler) {
     );
 }
 
+int execute_list_mode(NSMutableDictionary *handlers, NSString *current_handler_name) {
+    // List all HTTP handlers, marking the current one with a star
+    for (NSString *key in handlers) {
+        char *mark = [key isEqual:current_handler_name] ? "* " : "  ";
+            printf("%s%s\n", mark, [key UTF8String]);
+        }
+
+    return 0;
+}
+
+int execute_get_mode(NSString *current_handler_name) {
+    // Just ouput the current setting
+    printf("%s\n", [current_handler_name UTF8String]);
+
+    return 0;
+}
+
+int execute_set_mode(const char *target, NSMutableDictionary *handlers, NSString *current_handler_name) {
+    NSString *target_handler_name = [NSString stringWithUTF8String:target];
+
+    if ([target_handler_name isEqual:current_handler_name]) {
+      printf("%s is already set as the default HTTP handler\n", target);
+    } else {
+	NSString *target_handler = handlers[target_handler_name];
+
+	if (target_handler != nil) {
+	    // Set new HTTP handler (HTTP and HTTPS separately)
+	    set_default_handler(@"http", target_handler);
+	    set_default_handler(@"https", target_handler);
+	} else {
+	    printf("%s is not available as an HTTP handler\n", target);
+
+	    return 1;
+	}
+    }
+
+    return 0;
+}
+
 int main(int argc, const char *argv[]) {
     const char *target = (argc == 1) ? '\0' : argv[1];
+    int code;
     NSString *mode;
 
     if (target == '\0') {
@@ -65,40 +105,19 @@ int main(int argc, const char *argv[]) {
 
 	SWITCH( mode ) {
 		CASE (@"list") {
-		    // List all HTTP handlers, marking the current one with a star
-		    for (NSString *key in handlers) {
-			char *mark = [key isEqual:current_handler_name] ? "* " : "  ";
-			printf("%s%s\n", mark, [key UTF8String]);
-		    }
+		    code = execute_list_mode(handlers, current_handler_name);
 		    break;
 		}
 		CASE (@"get") {
-		    printf("%s\n", [current_handler_name UTF8String]);
+		    code = execute_get_mode(current_handler_name);
 		    break;
 		}
 		DEFAULT {
-		    NSString *target_handler_name = [NSString stringWithUTF8String:target];
-
-		    if ([target_handler_name isEqual:current_handler_name]) {
-		      printf("%s is already set as the default HTTP handler\n", target);
-		    } else {
-			NSString *target_handler = handlers[target_handler_name];
-
-			if (target_handler != nil) {
-			    // Set new HTTP handler (HTTP and HTTPS separately)
-			    set_default_handler(@"http", target_handler);
-			    set_default_handler(@"https", target_handler);
-			} else {
-			    printf("%s is not available as an HTTP handler\n", target);
-
-			    return 1;
-			}
-		    }
+		    code = execute_set_mode(target, handlers, current_handler_name);
 		    break;
 		}
-		return 0;
 	}
     }
 
-    return 0;
+    return code;
 }
